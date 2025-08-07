30644bd147dd0d972b3e0ed0f99c3ce5fa7597af
Damiano <114597691+damiano-g@users.noreply.github.com>
2025-08-07 14:14:01 +0200

    First attempt

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let revString;
+    let lastInd = String(string).length-1;
 
+    for(let i=0; i <= lastInd; i++){
+        String(revString) += String(string).charAt(lstInd-i);
+    }
+
+    return revString;
 };
 
 // Do not edit below this line
