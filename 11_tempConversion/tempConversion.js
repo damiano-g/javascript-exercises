ac93d717e31fe8120d6c0ba456638706f3997a51
Damiano <114597691+damiano-g@users.noreply.github.com>
2025-08-07 16:13:06 +0200

    Conversion formula fixed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(temp) {
-  return parseFloat(temp.toFixed(1)) - 32.0;
+  return (parseFloat(temp.toFixed(1)) - 32.0) * (5/9);
 };
 
 const convertToFahrenheit = function(temp) {
-  return parseFloat(temp.toFixed(1)) + 32.0;
+  return parseFloat(temp.toFixed(1)) * (9/5) + 32.0;
 };
 
 // Do not edit below this line
