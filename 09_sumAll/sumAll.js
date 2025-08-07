dade033fd90ff2261472da4a4c2d7d83f06ba9db
Damiano <114597691+damiano-g@users.noreply.github.com>
2025-08-07 15:13:43 +0200

    Previous changes and sumAll first attempt

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
 
+    if(
+        typeof numA != "number" ||
+        typeof numB != "number" ||
+        !Number.isInteger(numA) ||
+        !Number.isInteger(numB)
+    ) return "ERROR";
+
+    let minNum = Math.min(numA, numB);
+    let maxNum = Math.max(numA, numB);
+    let sum = minNum
+
+    while(minNum <= maxNum){
+        sum += ++minNum
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
