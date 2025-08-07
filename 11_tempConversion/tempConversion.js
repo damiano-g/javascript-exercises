ad78b3df316de9e8f66a645b9e36ceb1183cee34
Damiano <114597691+damiano-g@users.noreply.github.com>
2025-08-07 16:08:56 +0200

    Try adding float values (avoids casting)

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(temp) {
-  return parseFloat(temp.toFixed(1)) - 32;
+  return parseFloat(temp.toFixed(1)) - 32.0;
 };
 
 const convertToFahrenheit = function(temp) {
-  return parseFloat(temp.toFixed(1)) + 32;
+  return parseFloat(temp.toFixed(1)) + 32.0;
 };
 
 // Do not edit below this line
