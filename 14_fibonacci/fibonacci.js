a5b835b79f6e750ed3f3b1d002dd342ccdf71497
Damiano <114597691+damiano-g@users.noreply.github.com>
2025-08-11 15:20:30 +0200

    Solved

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    
+    position = Number(position);
 
+    if(position < 0) return "OOPS";
+    if(position == 0) return 0;
+    if(position == 1) return 1;
+
+    let prev = 0;
+    let actual = 1;
+    let res;
+
+    for(let i=2; i <= position; i++){
+        res = actual+prev;
+        prev = actual;
+        actual = res;
+    }
+
+    return res;
 };
 
 // Do not edit below this line
