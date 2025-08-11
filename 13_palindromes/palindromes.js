52d4b90e0dd28be5e4d2ec6847fc6258fed789bb
Damiano <114597691+damiano-g@users.noreply.github.com>
2025-08-11 13:00:17 +0200

    Solved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (myString) {
+    let aplhanum = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    let cleanString = myString.toLowerCase().split("").filter((char)=>aplhanum.includes(char)).join("");
+    let revString = cleanString.split("").reverse().join("");
+
+    return cleanString === revString;
 };
 
 // Do not edit below this line
